e89eea8e964164c94cb784e96890c4295922e510
StanaSimec <simecek.stanis@gmail.com>
2022-05-01 21:57:49 +0200

    results update

--- src/data/results.js
+++ src/data/results.js
@@ -3,6 +3,14 @@ import { TEAMS } from "../constants/constants";
 export const RESULTS = {
   A_TEAM: [
     {
+      round: 20,
+      date: new Date("2022-05-01"),
+      homeTeam: TEAMS.ZAROSICE,
+      homeScore: 0,
+      awayTeam: TEAMS.PETROV,
+      awayScore: 0,
+    },
+    {
       round: 19,
       date: new Date("2022-04-24"),
       homeTeam: TEAMS.DOLNI_BOJANOVICE,
@@ -157,6 +165,14 @@ export const RESULTS = {
   ],
   U19: [
     {
+      round: 18,
+      date: new Date("2022-05-01"),
+      homeTeam: TEAMS.KOBYLI_BORETICE,
+      homeScore: 5,
+      awayTeam: TEAMS.ZAROSICE,
+      awayScore: 2,
+    },
+    {
       round: 17,
       date: new Date("2022-04-24"),
       homeTeam: TEAMS.ZAROSICE,
@@ -262,6 +278,14 @@ export const RESULTS = {
     },
   ],
   U15: [
+    {
+      round: 18,
+      date: new Date("2022-05-01"),
+      homeTeam: TEAMS.ZAROSICE,
+      homeScore: 4,
+      awayTeam: TEAMS.PISEK_ZERAVICE,
+      awayScore: 1,
+    },
     {
       round: 17,
       date: new Date("2022-04-23"),
